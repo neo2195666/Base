a4f6e75c49a565dc7a77b727f9aa8f48e68dbdeb
neo2195357 <neo2195357@gmail.com>
2023-04-02 13:14:06 +0800

    锁子

---
+++ src/MyLock/MySynchronized.java
@@ -0,0 +1,33 @@
+package MyLock;
+
+//synchronized本质就是排队
+
+public class MySynchronized {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+
+        Ticket ticket = new Ticket();
+        new Thread( ()->{
+            for (int i = 0; i < 60; i++)
+                ticket.sale();
+        },"A").start();
+        new Thread( ()->{
+            for (int i = 0; i < 60; i++)
+                ticket.sale();
+        },"B").start();
+        new Thread( ()->{
+            for (int i = 0; i < 60; i++)
+                ticket.sale();
+        },"C").start();
+    }
+}
+
+
+class Ticket{
+    int ticket = 60;
+
+    public synchronized void sale(){
+        if(ticket > 0)
+            System.out.println(Thread.currentThread().getName() + "卖了一张票，剩余" + ticket-- + "票");
+    }
+}
